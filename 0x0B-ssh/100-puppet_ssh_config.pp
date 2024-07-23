e07042b6a539141da3c9caf2bcc5d7b771ef76eb
rmateus89 <raul.fm.mateus@gmail.com>
2024-07-23 15:14:33 -0700

    done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 # pupet file to make changes
 
+
 file { 'etc/ssh/ssh_cofig':
-	ensure => present,
-
+ensure => present,
 content =>"
-
 	#SSH client configuration
 	host*
 	IdentityFile ~/.ssh/school
 	PasswordAuthentication no
 	",
-
 }
